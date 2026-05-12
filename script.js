088debc08f7044590ea55c1506bddcf51e9056f7
cbonilla300 <127162107+cbonilla300@users.noreply.github.com>
2026-05-11 22:16:54 -0400

    improved responsiveness & removed unwated code/comments & moved js styling to css/improved eventListener

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const share = document.querySelector("#share-button");
-
+const rightSidePositionParent = document.querySelector(".right-side")
 share.addEventListener("click", (e) => {
     console.log(e.target.tagName)
-    let container = document.createElement("div");
-    let shareText = document.createElement("span");
+    const container = document.createElement("div");
+    container.className = "share-container"
+    const shareText = document.createElement("span");
     shareText.textContent = "SHARE";
-    let facebook = document.createElement("img");
-    facebook.setAttribute("src","imagesicon-twitter.svg");
-    let twitter = document.createElement("img");
-    twitter.setAttribute("src", "imagesicon-twitter.svg");
-    let pinterest = document.createElement("img");
-    pinterest.setAttribute("src", "imagesicon-pinterest.svg");
+    const facebook = document.createElement("img");
+    facebook.setAttribute("src","images/icon-facebook.svg");
+    const twitter = document.createElement("img");
+    twitter.setAttribute("src", "images/icon-twitter.svg");
+    const pinterest = document.createElement("img");
+    pinterest.setAttribute("src", "images/icon-pinterest.svg");
     document.body.appendChild(container);
     container.appendChild(shareText);
     container.appendChild(facebook);
     container.appendChild(twitter);
     container.appendChild(pinterest);
-    container.style.display = "flex";
-    container.style.width = "250px";
-    container.style.justifyContent = "space-evenly";
-    container.style.backgroundColor = "hsl(217, 19%, 35%)";
-    container.style.padding = ".7rem";
+    rightSidePositionParent.appendChild(container)
     console.log("test");
 })
 console.log("second console")
